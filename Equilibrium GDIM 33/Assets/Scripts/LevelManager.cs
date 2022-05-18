34c3759268667a30ef15ef615127e6a422ba327c
Quockerz <cromeis@uci.edu>
2022-05-18 00:57:41 -0700

    added UI and button for next level
    beating the level pops up a UI for next level, when button is pressed, it loads next scene

--- Equilibrium GDIM 33/Assets/Scripts/LevelManager.cs
+++ Equilibrium GDIM 33/Assets/Scripts/LevelManager.cs
@@ -9,11 +9,19 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private GameObject top_winPad;
     [SerializeField] private GameObject bottom_winPad;
-    [SerializeField] private string level_name;
+    [SerializeField] private string nextLevel_name;
+
+    [SerializeField] private GameObject nextlevelButton;
+    [SerializeField] private GameObject win_text;
 
     private SpriteRenderer top_sprite;
     private SpriteRenderer bottom_sprite;
 
+    private void Start()
+    {
+        nextlevelButton.SetActive(false); // set win buttons to not active when game starts
+        win_text.SetActive(false);
+    }
     public static void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -24,9 +32,16 @@ public class LevelManager : MonoBehaviour
         top_sprite = top_winPad.GetComponent<SpriteRenderer>();
         bottom_sprite = bottom_winPad.GetComponent<SpriteRenderer>();
 
-        if((top_sprite.color == Color.green) && (bottom_sprite.color == Color.green))
+        if((top_sprite.color == Color.green) && (bottom_sprite.color == Color.green)) //if win condition is met
         {
-            SceneManager.LoadScene(level_name);
+            Time.timeScale = 0; //pause game
+            win_text.SetActive(true); //make level complete text appear
+            nextlevelButton.SetActive(true); //make next level button appear
         }
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel_name);
     }
 }
