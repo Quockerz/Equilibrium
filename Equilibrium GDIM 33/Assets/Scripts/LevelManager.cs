27a1e6d8ce012d40bd3924f661a8bbb22c5d95dd
Quockerz <cromeis@uci.edu>
2022-05-18 00:37:37 -0700

    level manager updates to next level
    when both pads are green the next level is loaded

--- Equilibrium GDIM 33/Assets/Scripts/LevelManager.cs
+++ Equilibrium GDIM 33/Assets/Scripts/LevelManager.cs
@@ -6,8 +6,27 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private GameObject top_winPad;
+    [SerializeField] private GameObject bottom_winPad;
+    [SerializeField] private string level_name;
+
+    private SpriteRenderer top_sprite;
+    private SpriteRenderer bottom_sprite;
+
     public static void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update()
+    {
+        top_sprite = top_winPad.GetComponent<SpriteRenderer>();
+        bottom_sprite = bottom_winPad.GetComponent<SpriteRenderer>();
+
+        if((top_sprite.color == Color.green) && (bottom_sprite.color == Color.green))
+        {
+            SceneManager.LoadScene(level_name);
+        }
     }
 }
